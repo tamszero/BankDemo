a5db67d8bc328eaa96743995c85586fad0a1a72f
jihye <hanjihye68@ewhain.net>
2026-09-02 18:59:44 +0900

    í커밋테스트

--- src/main/java/com/example/demo/transaction/TransactionController.java
+++ src/main/java/com/example/demo/transaction/TransactionController.java
@@ -88,6 +88,8 @@ public class TransactionController {
 
         redirectAttributes.addFlashAttribute("message", "출금이 완료되었습니다");
         return "redirect:/accounts/" + withdrawRequest.getAccountId();
+
+        //커밋 테스트용 수정
     }
 
 }
