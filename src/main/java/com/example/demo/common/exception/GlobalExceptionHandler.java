1327d22d28908f8da0eeae32e11f20ace34af07f
tamszero <orenooshi@gmail.com>
2026-09-07 19:37:03 +0900

    feat:modify GlobalExceptionHandler to model view

--- src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/common/exception/GlobalExceptionHandler.java
@@ -4,17 +4,24 @@ package com.example.demo.common.exception;
 // ->  500 대신 제대로 된 상태코드/메시지를 받기 위해
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BuisinessException.class)
-    public ResponseEntity<String> handleBuisinessException(BuisinessException e){
+    public ModelAndView handleBuisinessException(BuisinessException e){
         ErrorCode errorCode = e.getErrorCode();
-        return ResponseEntity.status(errorCode.getStatus())
-                .body(errorCode.getMessage());
+
+        ModelAndView mav = new ModelAndView("error/buisiness");
+        mav.addObject("code", errorCode.getCode());
+        mav.addObject("message", errorCode.getMessage());
+        mav.setStatus(errorCode.getStatus());
+
+        return mav;
     }
 }
---
+++ src/main/webapp/WEB-INF/views/error/business.jsp
@@ -0,0 +1,14 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>오류</title>
+</head>
+<body>
+<h1>요청을 처리할 수 없습니다</h1>
+<p>${message}</p>
+<p style="color:gray; font-size:0.9em;">오류 코드: ${code}</p>
+<a href="/">홈으로</a>
+</body>
+</html>
